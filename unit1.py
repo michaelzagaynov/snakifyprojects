d8bcfb14ac8dd92a9cbd97de03aab61a5530f6a1
Michael Zagaynov <michael.zagaynov@student.rjuhsd.us>
2018-10-29 20:52:46 +0000

    aa

---
+++ unit1.py
@@ -0,0 +1,130 @@
+# This program reads two numbers and prints their sum:
+a = int(input())
+b = int(input())
+c = int(input())
+print(a + b + c)
+
+# Can you change it so it can add three numbers?
+
+
+
+
+a = input()
+print("Hi " + a )
+
+
+
+
+
+
+a = int(input())
+print(a ** 2)
+
+
+
+
+
+
+# Read the numbers b and h like this:
+b = int(input())
+h = int(input())
+print(((b *  h )) * .5)
+# Print the result with print()
+
+
+
+
+
+
+# Read the name:
+name = input()
+
+# Print the result using:
+# print('Greeting', name)
+print("Hello, "  + name + "!")
+
+
+
+
+
+
+
+
+
+# Read the numbers like this:
+n = int(input())
+k = int(input())
+# Print the result with print()
+
+# Example of division, integer division and remainder:
+print(k // n)
+print(k % n)
+
+
+
+
+
+
+
+
+
+# Read the numbers like this:
+n = int(input())
+k = int(input())
+# Print the result with print()
+
+# Example of division, integer division and remainder:
+print(k // n)
+print(k % n)
+
+
+
+
+
+
+
+
+hour1 = input()
+minute1 = input()
+second1 = input()
+h1 = (int(hour1) * 3600)
+m1 = (int(minute1) * 60)
+s1 = int(second1)
+hour2 = input()
+minute2 = input()
+second2 = input()
+h2 = (int(hour2) * 3600)
+m2 = (int(minute2) * 60)
+s2 = int(second2)
+total1 = int(h1 + m1 + s1)
+total2 = int(h2 + m2 + s2)
+answer = int(total2 - total1)
+print(answer)
+
+
+
+
+
+
+
+
+
+
+
+# Read an integer:
+# a = int(input())
+# Read a float:
+# b = float(input())
+# Print a value:
+# print(a, b)
+c1 = input()
+c2 = input()
+c3 = input()
+d1 = int(c1)//2
+d2 = int(c2)//2
+d3 = int(c3)//2
+l1 = int(c1)%2
+l2 = int(c2)%2
+l3 = int(c3)%2
+answer = (d1+d2+d3+l1+l2+l3)
+print(answer)
